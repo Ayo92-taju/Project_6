c394dfdcfc72a89c47e29a4577a179a5701f324c
Ayo92-taju <69106007+Ayo92-taju@users.noreply.github.com>
2026-01-30 02:32:42 +0100

    Refactor contact management to use classes

--- contactList.py
+++ contactList.py
@@ -1,118 +1,144 @@
-import string
-contacts = []
-
-def menu():
-    while True:
-        print("Select option: ")
-        print("1. Add Contact")
-        print("2. View Contacts")
-        print("3. Search Contacts")
-        print("4. Delete Contact")
-        print("5. Edit Contact")
-        print("6. Exit")
-        select = int(input())
-
-        if select == 1:
-            add(contacts)
-            continue
-        elif select == 2:
-            view(contacts)
-            continue
-        elif select == 3:
-            search(contacts)
-            continue
-        elif select == 4:
-            delete(contacts)
-            continue
-        elif select == 5:
-            edit(contacts)
-        elif select == 6:
-            break
-        else:
-            print("Please select 1-5 from the options provided")
-            continue
+class Contact:
+    def __init__(self, name, numebr):
+        self.name = name
+        self.number = numebr
         
-def add(contacts):
-    try:
-        number = int(input("Enter number: "))
-        name = input("Enter contact name: ")
-        contact = {"Name": name, "Number": number}
-        print("Contact added!")
-        return contacts.append(contact)
+    def display(self):
+        print(f"{self.name} | {self.number}")
         
-    except ValueError:
-        print("Invalid input")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}\n")
-
-def view(contacts):
-    if not contacts:
-        print("No contacts added")
-    else:
+class ContactManager:
+    def __init__(self):
+        self.contacts = []
+    
+    def add_contact(self):
         try:
-            for i, contact in enumerate(contacts, start = 1):
-                print(f"{i}. {contact["Name"]} | {contact["Number"]}")
+            number = int(input("Enter number: "))
+            name = input("Enter contact name: ")
+            
+            contact = Contact(name, number)
+            self.contacts.append(contact)
+            print("Contact added!")
+        
+        except ValueError:
+            print("Invalid input")
         except Exception as e:
             print(f"An unexpected error occurred: {e}\n")
-        
-def delete(contacts):
-    if not contacts:
-        print("No contacts to delete")
-    else:
-        while True:
-            for i, contact in enumerate(contacts, start = 1):
-                print(f"{i}. {contact["Name"]} | {contact["Number"]}")
-            con = int(input("Enter contact ID you would like to delete: "))
-            if 1 <= con <= len(contacts):
-                removed = contacts.pop(con - 1)
-                print(f"Removed {removed["Name"]}")
-                break
-            elif con == 0:
-                break
-            else:
-                print("Please select a contact ID from the list provided")
-                continue
-            
-def edit(contacts):
-    if not contacts:
-        ("No contacts")
-    else:
-        while True:
-            for i, contact in enumerate(contacts, start = 1):
-                print(f"{i}. {contact["Name"]} | {contact["Number"]}")
-            con = int(input("Enter contact ID you would like to edit: "))
-            new_num = int(input("Enter number: "))
-            new_name = input("Enter contact name: ")
-            if 1 <= con <= len(contacts):
-                update = {"Name": new_name, "Number": new_num}
-                contacts[con - 1] = update
-                
-                print(f"Updated {update["Name"]}")
-                break
-            elif con == 0:
-                break
-            else:
-                print("Please select a contact ID from the list provided")
-                continue
-        
-            
-def search(contacts):
-    while True:
-        if not contacts:
-            print("No contacts entered")
-            break
+    
+    def view_contacts(self):
+        if not self.contacts:
+            print("No contacts added")
         else:
-            find_name = input("Please enter name to search: ").lower()
+            print("\nContact List:")
+            for i, contact in enumerate(self.contacts, start = 1):
+                print(f"{i}. {contact.name} | {contact.number}")
+    
+    def search_contact(self):
+        if not self.contacts:
+            print("No contacts entered")
+        else:
+            try:
+                find_name = input("Please enter name to search: ").lower()
+                
+                for contact in self.contacts:
+                    if find_name in contact.name.lower():
+                        print(f"{contact.name} | {contact.number}")
+                    else:
+                        print("Contact not found")
             
-            for contact in contacts:
-                if find_name in contact["Name"].lower():
-                    print(f"{contact["Name"]} | {contact["Number"]}")
-                    found = True
-                else:
-                    found = False
-            if not found:
-                    print("Contact not found")
+            except ValueError:
+                print("Invalid input")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}\n")
+            
+    
+    def edit_contact(self):
+        if not self.contacts:
+            print("No contacts")
+        
+        else:
+            try:
+                while True:
+                    for i, contact in enumerate(self.contacts, start = 1):
+                        print(f"{i}. {contact.name} | {contact.number}")
+                    con = int(input("Enter contact ID you would like to edit: "))
+                    number = int(input("Enter number: "))
+                    name = input("Enter contact name: ")
+                    if 1 <= con <= len(self.contacts):
+                        update = Contact(name, number)
+                        self.contacts[con - 1] = update
+                        
+                        print(f"Updated {update.name}")
+                        break
+                    elif con == 0:
+                        break
+                    else:
+                        print("Please select a contact ID from the list provided")
+                        continue
+            
+            except ValueError:
+                print("Invalid input")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}\n")
+                
+    def delete_contact(self):
+        if not self.contacts:
+            print("No contacts to delete")
+        else:
+            try:
+                while True:
+                    for i, contact in enumerate(self.contacts, start = 1):
+                        print(f"{i}. {contact.name} | {contact.number}")
+                    
+                    con = int(input("Enter contact ID you would like to delete: "))
+                    if 1 <= con <= len(self.contacts):
+                        print(f"{contact.name(con - 1)} deleted!")
+                        self.contacts.pop(con - 1)
+                        break
+                    elif con == 0:
+                        break
+                    else:
+                        print("Please select a contact ID from the list provided")
+                        continue
+            
+            except ValueError:
+                print("Invalid input")
+    
+    def menu(self):
+        while True:
+            print("Select option: ")
+            print("1. Add Contact")
+            print("2. View Contacts")
+            print("3. Search Contacts")
+            print("4. Delete Contact")
+            print("5. Edit Contact")
+            print("6. Exit")
+            
+            try:
+                select = int(input())
+
+                if select == 1:
+                    self.add_contact()
+                
+                elif select == 2:
+                    self.view_contacts()
+                    
+                elif select == 3:
+                    self.search_contact()
+                    
+                elif select == 4:
+                    self.delete_contact()
+                    
+                elif select == 5:
+                    self.edit_contact()
+                elif select == 6:
                     break
-            break
-print("Contact List\n")
-menu()
+                else:
+                    print("Please select 1-5 from the options provided")
+                    continue
+            except ValueError:
+                print("Invalid input")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}\n")
+                
+manager = ContactManager()
+manager.menu()
